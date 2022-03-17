69f8d0ed85d56cc3428a70fe974b3ce4ba85696f
phantomwofl <phantomwolf@yandex.ru>
2022-03-17 19:47:11 +0400

    origin

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,48 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.html.Option;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+// Stub
+public class PostRepository {
+
+  private long counter = 0;
+  private ConcurrentHashMap <Long, Post> collection;
+
+  public ConcurrentHashMap <Long, Post> all() {
+    return this.collection;
+  }
+
+  public Optional<Post> getById(long id) {
+    if (collection.containsKey(id)) {
+      return Optional.of(collection.get(id));
+    } else {
+      return Optional.empty();
+    }
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0) {
+      counter++;
+      collection.put(counter, post);
+    } else if (collection.containsKey(post.getId())) {
+      collection.put(post.getId(), post);
+    } else {
+     // HttpServletResponse.SC_NOT_FOUND;
+    }
+    return collection.get(post.getId());
+  }
+
+  public void removeById(long id) {
+    collection.remove(id);
+  }
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,57 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+
+  @Override
+  public void init() {
+    final var repository = new PostRepository();
+    final var service = new PostService(repository);
+    controller = new PostController(service);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    // если деплоились в root context, то достаточно этого
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+      final var ADDRESS = "/api/posts";
+      // primitive routing
+
+      if (method.equals("GET") && path.equals(ADDRESS)) {
+        controller.all(resp);
+        return;
+      }
+      if (method.equals("GET") && path.matches(ADDRESS + "/\\d+")) {
+        // easy way
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals("POST") && path.equals(ADDRESS)) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals("DELETE") && path.matches(ADDRESS + "/\\d+")) {
+        // easy way
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+}
+
