264dbfbba46b39cd2bb3b2ac41f7122ed0391d39
phantomwofl <phantomwolf@yandex.ru>
2022-03-31 16:37:03 +0400

    ver1.02

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -18,8 +18,6 @@ public class MainServlet extends HttpServlet {
     public void init() {
         final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
         controller = context.getBean(PostController.class);
-        final var service = context.getBean(PostService.class);
-        final var isSame = service == context.getBean("postService");
     }
 
     @Override
